4b41b0c6ac943bca07861afee26dd2a9e417cd55
will-terra <wpterra@outlook.com>
2025-03-21 14:29:37 -0300

    Feat: Initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Counter } from "./components/counter/Counter";
 
 export default function IndexPage() {
-  return <Counter />;
+  return <div> Skip</div>;
 }
 
 export const metadata: Metadata = {
