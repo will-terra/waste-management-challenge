9c8aa8554780ed5874afc904dc620f1f97f98cc0
will-terra <wpterra@outlook.com>
2025-03-22 19:46:30 -0300

    Feat: Responsive Handler

---
+++ components/utils/ResponsiveHandler.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { useEffect } from 'react';
+import { useAppDispatch } from '@/lib/hooks';
+import { setIsMobile } from '@/lib/features/skips/skipsSlice';
+
+export function ResponsiveHandler() {
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        const updateIsMobile = () => {
+            dispatch(setIsMobile(window.innerWidth <= 768));
+        };
+        updateIsMobile();
+        window.addEventListener('resize', updateIsMobile);
+        return () => {
+            window.removeEventListener('resize', updateIsMobile);
+        };
+    }, [dispatch]);
+    return null;
+}
