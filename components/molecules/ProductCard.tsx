9df23f2135b607e09e0616ab447cbb09b6c1f6e0
will-terra <wpterra@outlook.com>
2025-03-22 14:57:28 -0300

    Feat: Product card

--- components/molecules/ProductCard.tsx
+++ components/molecules/ProductCard.tsx
@@ -1,12 +1,15 @@
-import { Skip } from '@/app/types/types'
+import Image from 'next/image'
+import skipImage from '../../public/skip.png'
+import { Skip } from '@/types/types'
 
 export const ProductCard = (product: Skip) => {
     return (
-        <>
-            <div> Id : {product.id}</div>
+        <div className=" flex flex-col bg-white p-4 rounded-md shadow-md">
+            <Image src={skipImage} alt="skip" width={300} height={300} />
+            <p className="text-2xl font-bold"> {product.size} Yard Skip</p>
             <div>Allowed On Road= {product.allowed_on_road ? "Yes" : "No"}</div>
             <div> Allows heavy waste = {product.allows_heavy_waste ? "Yes" : "No"} </div>
-        </>
+        </div>
     )
 }
 
