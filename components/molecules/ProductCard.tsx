6f1f7123fa3673633751250d196ce1ee10f9a518
will-terra <wpterra@outlook.com>
2025-03-22 22:46:26 -0300

    Style: productCard Image priority set to reduce LCP

--- components/molecules/ProductCard.tsx
+++ components/molecules/ProductCard.tsx
@@ -5,7 +5,17 @@ import { Skip } from '@/types/types'
 export const ProductCard = (product: Skip) => {
     return (
         <div className=" flex flex-col bg-white p-4 rounded-md shadow-md">
-            <Image src={skipImage} alt="skip" width={300} height={300} />
+            <Image
+                priority
+                src={skipImage}
+                alt="skip"
+                width={300}
+                height={200}
+                style={{
+                    width: '100%',
+                    height: 'auto'
+                }}
+            />
             <p className="text-2xl font-bold"> {product.size} Yard Skip</p>
             <div>Allowed On Road= {product.allowed_on_road ? "Yes" : "No"}</div>
             <div> Allows heavy waste = {product.allows_heavy_waste ? "Yes" : "No"} </div>
