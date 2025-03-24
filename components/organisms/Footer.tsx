1debb73f1a111dea11382d4deb1be60def6effee
will-terra <wpterra@outlook.com>
2025-03-24 11:32:05 -0300

    Style: Footer responsive design

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -14,8 +14,8 @@ export const Footer = () => {
     return (
         <>
             {selectedSkip && (
-                <div className="fixed bottom-0 left-0 right-0 flex justify-end gap-10 bg-secondaryDarkGray border-t border-lightGray p-4 animate-slideUp z-50">
-                    <div className="flex items-center gap-4">
+                <footer className="fixed bottom-0 left-0 right-0 flex flex-col md:flex-row justify-end gap-4 md:gap-10 bg-secondaryDarkGray border-t border-lightGray py-3 px-4 animate-slideUp z-50">
+                    <div className="flex justify-center items-center gap-4">
                         <p className=" text-white text-2xl font-bold"> {size} Yard Skip</p>
                         <p className="text-4xl font-black text-lightBlue">£{price_before_vat}<span className="text-sm text-gray-400 ">per week</span></p>
                     </div>
@@ -23,7 +23,7 @@ export const Footer = () => {
                         <MainButton ariaLabel="Cancel Skip Selection" variant="gray" size="large" className="px-4" onClick={handleOnClick}>Cancel</MainButton>
                         <MainButton ariaLabel={`Continue with ${size} Yard selected Skip`} variant="blue" size="large">Continue</MainButton>
                     </div>
-                </div>
+                </footer>
             )}
         </>
     )
