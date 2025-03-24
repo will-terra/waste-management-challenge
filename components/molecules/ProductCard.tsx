9755f4122b8bd043eab5ccf9d22ee1f22db735bb
will-terra <wpterra@outlook.com>
2025-03-24 12:03:58 -0300

    Feat: remove product already selected

--- components/molecules/ProductCard.tsx
+++ components/molecules/ProductCard.tsx
@@ -11,19 +11,22 @@ export const ProductCard = (product: Skip) => {
     const { allows_heavy_waste, allowed_on_road, price_before_vat } = product
     const selectedSkip = useAppSelector(state => state.skips.selectedSkip)
     const dispatch = useAppDispatch()
-    const handleOnClick = () => {
+    const handleOnClick = (product: Skip) => {
+        if (selectedSkip?.id === product.id) {
+            dispatch(setSelectedSkip(null))
+            return
+        }
         dispatch(setSelectedSkip(product))
     }
     return (
-        <div onClick={handleOnClick} className={`flex flex-col gap-2 rounded-lg border-6 p-4 md:p-6 hover:border-lightBlue/50 border-lightGray text-white ${selectedSkip?.id === product.id ? "bg-lightBlue/30" : "bg-secondaryDarkGray"}`}>
+        <div onClick={() => handleOnClick(product)}
+            className={`flex flex-col gap-2 rounded-lg border-6 p-4 md:p-6 hover:border-lightBlue/50 border-lightGray text-white ${selectedSkip?.id === product.id ? "bg-lightBlue/30" : "bg-secondaryDarkGray"}`}>
             <Image
                 priority
                 src={skipImage}
                 alt="Skip with garbage"
-                width={255}
-                height={200}
                 style={{
-                    width: "100%",
+                    width: 255,
                     height: "auto"
                 }}
             />
@@ -31,9 +34,9 @@ export const ProductCard = (product: Skip) => {
             <div className="flex justify-between gap-2 min-h-12 items-center">
                 <p className="text-4xl font-black text-lightBlue">£{price_before_vat}<span className="text-sm text-gray-400 ">per week</span></p>
                 <div className="flex w-fit gap-1 bg-gray-400 rounded-md items-center">
-                    {allows_heavy_waste && <Image src={heavyIcon} aria-label="Allows heavy waste" alt="heavy waste icon" width={40} height={40} />}
+                    {allows_heavy_waste && <Image src={heavyIcon} aria-label="Allows heavy waste" alt="heavy waste icon" style={{ width: 40, height: "auto" }} />}
                     {allows_heavy_waste && allowed_on_road && <AddIcon />}
-                    {allowed_on_road && <Image src={roadIcon} aria-label="Allowed on road" alt="road icon" width={40} height={40} />}</div>
+                    {allowed_on_road && <Image src={roadIcon} aria-label="Allowed on road" alt="road icon" style={{ width: 40, height: "auto" }} />}</div>
             </div></div>
     )
 }
